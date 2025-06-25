d458fb439539a7757f7e5c2ed01f4e7c34d49faf
Adir Noyman <ofahol2007@hotmail.com>
2025-06-25 23:28:04 +0300

    .

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -14,3 +14,15 @@ class Cart():
 
         # If the user already has a cart, we use it, else the cart will be empty dict
         self.cart = cart
+    
+    def add(self, product, product_qty):
+        """
+        Add product to the cart or update its quantity if it already exists.
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            self.cart[product_id]['qty'] += product_qty
+        else:
+            self.cart[product_id] = {'qty': product_qty, 'price': str(product.price)}
+        # Mark the session as modified to ensure it is saved
+        self.session.modified = True
--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from .cart import Cart
+from store.models import Product
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
 
 def cart_summary(request):
     return render(request, "cart/cart-summary.html")
 
-def cart_add(request, product_id):
-    # Logic to add product to cart
-    return render(request, "cart/cart-add.html", {"product_id": product_id})
+def cart_add(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_quantity = int(request.POST.get('product_quantity'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product, product_qty=product_quantity)
+        return JsonResponse({"Product name": product.title, "Product quantity": product_quantity}, status=200)
 
 def cart_update(request, product_id):
     # Logic to update product quantity in cart
